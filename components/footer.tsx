fe7e6d9e7b2114c50667f474e13f30980b9f8cbf
Victorfernandezsaez <v.ferna.saez@gmail.com>
2023-11-20 13:16:49 +0100

    footer mb fixed

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className='mb-10 px-4 text-center text-gray-500'>
+        <footer className='sm:mb-10 mb-14 px-4 text-center text-gray-500'>
             <small className='mb-2 text-xs'>
                 &copy; 2023 Victor Fernández Sáez. All rights reserved.
             </small>
